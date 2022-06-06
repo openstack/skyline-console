3b2144c39d084c0b61192b456e51ed2c8168f5c4
xusongfu <xu.songfu@99cloud.net>
2022-06-06 13:25:54 +0800

    test: Add zun e2e test
    1. create zun container test
    2. start zun container test
    3. pause zun container test
    4. unpause zun container test
    5. reboot zun container test
    6. stop zun container test
    7. delete zun container test
    8. create zun capsule test
    9. delete zun capsule test
    
    Change-Id: If291bbfd1dde4a73b5e14c171e3bb885d687f480

---
+++ test/e2e/integration/pages/zun/capsule.spec.js
@@ -0,0 +1,36 @@
+import { onlyOn } from '@cypress/skip-test';
+import { zunCapsuleListUrl } from '../../../support/constants';
+
+const zunServiceEnabled = (Cypress.env('extensions') || []).includes('zun');
+
+onlyOn(!zunServiceEnabled, () => {
+  describe('Skip The zunCapsule Page', () => {
+    it('successfully skip', () => {});
+  });
+});
+
+onlyOn(zunServiceEnabled, () => {
+  describe('The zunCpsule Page', () => {
+    const listUrl = zunCapsuleListUrl;
+    const filename = 'zunCapsuleTemplate.yaml';
+    const zunCapsuleName = 'e2e-zun-capsule';
+
+    beforeEach(() => {
+      cy.login(listUrl);
+    });
+
+    it('successfully create', () => {
+      cy.clickHeaderButton(1)
+        .formAttachFile('template_file', filename)
+        .clickModalActionSubmitButton();
+    });
+
+    it('successfully delete', () => {
+      cy.tableSimpleSearchText(zunCapsuleName)
+        .checkTableFirstRow(zunCapsuleName)
+        .clickFirstActionButton()
+        .clickConfirmActionSubmitButton()
+        .checkEmptyTable();
+    });
+  });
+});
---
+++ test/e2e/integration/pages/zun/container.spec.js
@@ -0,0 +1,95 @@
+import { onlyOn } from '@cypress/skip-test';
+import { zunContainerListUrl } from '../../../support/constants';
+
+const zunServiceEnabled = (Cypress.env('extensions') || []).includes('zun');
+
+onlyOn(!zunServiceEnabled, () => {
+  describe('Skip The zunContainer Page', () => {
+    it('successfully skip', () => {});
+  });
+});
+
+onlyOn(zunServiceEnabled, () => {
+  describe('The zunContainer Page', () => {
+    const listUrl = zunContainerListUrl;
+    const uuid = Cypress._.random(0, 1e6);
+    const zunContainerName = `e2e-zunContainer-${uuid}`;
+
+    beforeEach(() => {
+      cy.login(listUrl);
+    });
+
+    it('successfully create', () => {
+      cy.clickHeaderButton(1)
+        .url()
+        .should('include', `${listUrl}/create`)
+        .wait(5000)
+        .formInput('containerName', zunContainerName)
+        .formInput('image', 'cirros')
+        .clickStepActionNextButton()
+        .wait(2000)
+        .clickStepActionNextButton()
+        .wait(2000)
+        .clickStepActionNextButton()
+        .wait(2000)
+        .clickStepActionNextButton()
+        .wait(2000)
+        .clickStepActionNextButton()
+        .waitFormLoading()
+        .url()
+        .should('include', listUrl)
+        .closeNotice()
+        .waitStatusTextByFresh('Created');
+    });
+
+    it('successfully start', () => {
+      cy.tableSimpleSearchText(zunContainerName)
+        .checkTableFirstRow(zunContainerName)
+        .clickActionInMore('Start')
+        .clickConfirmActionSubmitButton()
+        .waitStatusTextByFresh('Running');
+    });
+
+    it('successfully pause', () => {
+      cy.tableSimpleSearchText(zunContainerName)
+        .checkTableFirstRow(zunContainerName)
+        .clickActionInMore('Pause')
+        .clickConfirmActionSubmitButton()
+        .waitStatusTextByFresh('Paused');
+    });
+
+    it('successfully unpause', () => {
+      cy.tableSimpleSearchText(zunContainerName)
+        .checkTableFirstRow(zunContainerName)
+        .clickActionButtonByTitle('Unpause')
+        .clickConfirmActionSubmitButton()
+        .waitStatusTextByFresh('Running');
+    });
+
+    it('successfully reboot', () => {
+      cy.tableSimpleSearchText(zunContainerName)
+        .checkTableFirstRow(zunContainerName)
+        .clickActionInMore('Reboot')
+        .clickConfirmActionSubmitButton()
+        .waitStatusTextByFresh('Restarting')
+        .wait(5000)
+        .waitStatusTextByFresh('Running');
+    });
+
+    it('successfully stop', () => {
+      cy.tableSimpleSearchText(zunContainerName)
+        .checkTableFirstRow(zunContainerName)
+        .clickActionInMore('Stop')
+        .clickConfirmActionSubmitButton()
+        .waitStatusTextByFresh('Stopped');
+    });
+
+    it('successfully delete', () => {
+      cy.tableSimpleSearchText(zunContainerName)
+        .checkTableFirstRow(zunContainerName)
+        .clickFirstActionButton()
+        .clickConfirmActionSubmitButton()
+        .checkEmptyTable();
+    });
+  });
+});
