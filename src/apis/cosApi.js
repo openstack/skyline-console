4e8ecafb0f97a124f771dacc03b73be610830244
Lizzy Liang <lizzy.liang@bigstack.co>
2025-08-05 12:04:46 +0800

    feat: add tokens api
    Signed-off-by: Lizzy Liang <lizzy.liang@bigstack.co>

--- src/apis/cosApi.js
+++ src/apis/cosApi.js
@@ -1,5 +1,15 @@
 import axios from 'axios';
+import { getToken } from './cosTokenApi';
 
 export const cosApiClientV1 = axios.create({
   baseURL: '/cos-api/v1',
+});
+
+cosApiClientV1.interceptors.request.use(async (config) => {
+  if (config.url.endsWith('/datacenters')) {
+    return config;
+  }
+  const token = await getToken();
+  config.headers.Authorization = `Bearer ${token}`;
+  return config;
 });
