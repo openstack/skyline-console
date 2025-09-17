90fbc1ce0822f3bd5afb93533b6cb223a1ad368b
Lizzy Liang <lizzy.liang@bigstack.co>
2025-09-17 09:58:45 +0800

    feat: reuse OS mapping values and update ImageType component
    Signed-off-by: Lizzy Liang <lizzy.liang@bigstack.co>

--- src/components/ImageType/index.jsx
+++ src/components/ImageType/index.jsx
@@ -12,75 +12,37 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import React, { Component } from 'react';
-import classnames from 'classnames';
+import React from 'react';
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
 import { upperFirst } from 'lodash';
 import { Tooltip } from 'antd';
-import centosIcon from 'asset/image/centos.svg';
-import ubuntuIcon from 'asset/image/ubuntu.svg';
-import fedoraIcon from 'asset/image/fedora.svg';
-import windowsIcon from 'asset/image/windows.svg';
-import debianIcon from 'asset/image/debian.svg';
-import coreosIcon from 'asset/image/coreos.svg';
-import archIcon from 'asset/image/arch.svg';
-import freebsdIcon from 'asset/image/freebsd.svg';
-import othersIcon from 'asset/image/others.svg';
+import { osIconMap, osTitleMap } from 'utils/os';
+import othersIcon from 'image/others.svg';
 import styles from './index.less';
 
-export default class index extends Component {
-  static propTypes = {
-    type: PropTypes.string,
-    className: PropTypes.string,
-    title: PropTypes.string,
-  };
+const Index = ({ type, className, title: titleProp }) => {
+  const IconComponent = osIconMap[type?.toLowerCase()] || othersIcon;
 
-  static defaultProps = {
-    className: '',
-  };
+  const title = osTitleMap[titleProp?.toLowerCase()] || upperFirst(titleProp);
 
-  constructor(props) {
-    super(props);
-    this.icons = {
-      centos: centosIcon,
-      ubuntu: ubuntuIcon,
-      fedora: fedoraIcon,
-      windows: windowsIcon,
-      debian: debianIcon,
-      coreos: coreosIcon,
-      arch: archIcon,
-      freebsd: freebsdIcon,
-      // The following two are for compatibility
-      other: othersIcon,
-      others: othersIcon,
-    };
-    this.titles = {
-      centos: 'CentOS',
-      ubuntu: 'Ubuntu',
-      fedora: 'Fedora',
-      windows: 'Windows',
-      debian: 'Debian',
-      coreos: 'CoreOS',
-      arch: 'Arch',
-      freebsd: 'FreeBSD',
-      // The following two are for compatibility
-      other: 'Others',
-      others: 'Others',
-    };
-  }
+  const iconElement = (
+    <IconComponent className={classnames(styles.image, className)} />
+  );
 
-  render() {
-    const { type, className, title: titleProp } = this.props;
+  return title ? <Tooltip title={title}>{iconElement}</Tooltip> : iconElement;
+};
 
-    const IconComponent = this.icons[type?.toLowerCase()] || othersIcon;
+Index.propTypes = {
+  type: PropTypes.string,
+  className: PropTypes.string,
+  title: PropTypes.string,
+};
 
-    const title =
-      this.titles[titleProp?.toLowerCase()] || upperFirst(titleProp);
+Index.defaultProps = {
+  type: '',
+  className: '',
+  title: '',
+};
 
-    const iconElement = (
-      <IconComponent className={classnames(styles.image, className)} />
-    );
-
-    return title ? <Tooltip title={title}>{iconElement}</Tooltip> : iconElement;
-  }
-}
+export default Index;
